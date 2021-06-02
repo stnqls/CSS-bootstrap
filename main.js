f8c410aa382a8ab08eaf613e995486fe9799a76a
stnqls <stnqls3938@gmail.com>
2021-06-02 19:46:33 +0900

    npm project - customized

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
-//tooltip은 사용전에 초기화를 해줘야 작동한다.
-
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle'
