a5b3509e1df7372931d1184d317c45684f454035
stnqls <stnqls3938@gmail.com>
2021-06-02 20:09:35 +0900

    Optimize

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-import bootstrap from 'bootstrap/dist/js/bootstrap.bundle'
+import Dropdown from 'bootstrap/js/dist/dropdown'
+import Modal from 'bootstrap/js/dist/modal'
+
+//초기화
+const dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+dropdownElementList.map(function (dropdownToggleEl) {
+  return new Dropdown(dropdownToggleEl)
+})
+
+new Modal(document.getElementById('#exampleModal'), {
+  backdrop : 'static'
+})
